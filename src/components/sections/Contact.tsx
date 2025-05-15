2fa3cda9797df58142362801ee5a41c586f0a7ac
root <muchellealvin@gmail.com>
2025-05-15 23:58:39 +0300

    Update About Section

--- src/components/sections/Contact.tsx
+++ src/components/sections/Contact.tsx
@@ -5,6 +5,7 @@ import {
   faLinkedinIn,
 } from "@fortawesome/free-brands-svg-icons";
 import { faEnvelope } from "@fortawesome/free-solid-svg-icons";
+import { GradientText } from "../GradientText";
 
 export const Contact: React.FC = () => {
   return (
@@ -13,7 +14,9 @@ export const Contact: React.FC = () => {
       className="flex flex-col items-center justify-center py-12" 
     >
       <div className="text-center">
-        <h2 className="text-2xl font-bold mb-6 text-primary">Let's link!</h2>
+        <h2 className="text-3xl font-bold mb-8 text-center">
+          <GradientText>Let's link!</GradientText>
+        </h2>
         <div className="flex justify-center space-x-6 mb-8"> 
           <a 
             href="mailto:muchellealvin@gmail.com"
